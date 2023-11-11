5ca0aef379461879b8104bfa58100044bf604591
proft <proftua@gmail.com>
2023-11-11 09:50:49 +0200

    Initial Commit

---
+++ LiveActivityExample/LiveActivityExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityExampleApp.swift
+//  LiveActivityExample
+//
+//  Created by Ivan Morgun on 11.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivityExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
